d9937e9040d463e3744d41060d19e98033ffb5c0
Matheus Araujo <matheus.saraujo@gmail.com>
2024-05-14 20:56:31 +0200

    feat(create-transaction): sending data to api

--- src/app/create-transaction/create-transaction.component.ts
+++ src/app/create-transaction/create-transaction.component.ts
@@ -2,14 +2,23 @@ import { Component } from "@angular/core";
 import { InputTextModule } from "primeng/inputtext";
 import { FormsModule } from "@angular/forms";
 import { FloatLabelModule } from "primeng/floatlabel";
+import { TransactionsService } from "../transactions.service";
+import { ButtonModule } from "primeng/button";
 
 @Component({
   selector: "app-create-transaction",
   standalone: true,
-  imports: [FormsModule, InputTextModule, FloatLabelModule],
+  imports: [FormsModule, InputTextModule, FloatLabelModule, ButtonModule],
   templateUrl: "./create-transaction.component.html",
   styleUrl: "./create-transaction.component.scss",
 })
 export class CreateTransactionComponent {
   value: string | undefined;
+
+  constructor(private service: TransactionsService) {}
+
+  onSubmit(): void {
+    this.service.createTransaction(this.value);
+    window.location.reload(); // TODO: improve this!!!
+  }
 }
