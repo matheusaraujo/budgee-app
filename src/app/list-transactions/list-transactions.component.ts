8038272ea7bd29885e07e7e8506daf59bc645faf
Matheus Araujo <matheus.saraujo@gmail.com>
2024-05-14 20:50:49 +0200

    feat(list-transactions): fetching data from api

--- src/app/list-transactions/list-transactions.component.ts
+++ src/app/list-transactions/list-transactions.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { TableModule } from "primeng/table";
 import { CommonModule } from "@angular/common";
+import { TransactionsService } from "../transactions.service";
 
 @Component({
   selector: "app-list-transactions",
@@ -10,46 +11,13 @@ import { CommonModule } from "@angular/common";
   styleUrl: "./list-transactions.component.scss",
 })
 export class ListTransactionsComponent implements OnInit {
-  products!: any[];
+  transactions!: any[];
+
+  constructor(private service: TransactionsService) {}
 
   ngOnInit() {
-    this.products = [
-      {
-        id: "1000",
-        code: "f230fh0g3",
-        name: "Bamboo Watch",
-        description: "Product Description",
-        image: "bamboo-watch.jpg",
-        price: 65,
-        category: "Accessories",
-        quantity: 24,
-        inventoryStatus: "INSTOCK",
-        rating: 5,
-      },
-      {
-        id: "1000",
-        code: "f230fh0g3",
-        name: "Bamboo Watch",
-        description: "Product Description",
-        image: "bamboo-watch.jpg",
-        price: 65,
-        category: "Accessories",
-        quantity: 24,
-        inventoryStatus: "INSTOCK",
-        rating: 5,
-      },
-      {
-        id: "1000",
-        code: "f230fh0g3",
-        name: "Bamboo Watch",
-        description: "Product Description",
-        image: "bamboo-watch.jpg",
-        price: 65,
-        category: "Accessories",
-        quantity: 24,
-        inventoryStatus: "INSTOCK",
-        rating: 5,
-      },
-    ];
+    this.service.getTransactions().subscribe((response) => {
+      this.transactions = response as [];
+    });
   }
 }
