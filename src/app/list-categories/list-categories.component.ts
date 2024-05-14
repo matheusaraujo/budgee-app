5a133566a822865220ada3c95cfe44dbffb85505
Matheus Araujo <matheus.saraujo@gmail.com>
2024-05-14 20:45:10 +0200

    feat(list-categories): fetching data from api

--- src/app/list-categories/list-categories.component.ts
+++ src/app/list-categories/list-categories.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ChartModule } from "primeng/chart";
+import { TransactionsService } from "../transactions.service";
 
 @Component({
   selector: "app-list-categories",
@@ -12,38 +13,32 @@ export class ListCategoriesComponent implements OnInit {
   data: any;
   options: any;
 
+  constructor(private service: TransactionsService) {}
+
   ngOnInit() {
-    const documentStyle = getComputedStyle(document.documentElement);
-    const textColor = documentStyle.getPropertyValue("--text-color");
+    this.service.getTransactionsByCategory().subscribe((response) => {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue("--text-color");
 
-    this.data = {
-      labels: ["Category 1", "Category 2", "Category 3"],
-      datasets: [
-        {
-          data: [300, 58, 100],
-          backgroundColor: [
-            documentStyle.getPropertyValue("--blue-500"),
-            documentStyle.getPropertyValue("--yellow-500"),
-            documentStyle.getPropertyValue("--green-500"),
-          ],
-          hoverBackgroundColor: [
-            documentStyle.getPropertyValue("--blue-400"),
-            documentStyle.getPropertyValue("--yellow-400"),
-            documentStyle.getPropertyValue("--green-400"),
-          ],
-        },
-      ],
-    };
+      this.data = {
+        labels: (response as []).map((r: any) => r.category),
+        datasets: [
+          {
+            data: (response as []).map((r: any) => r.amount),
+          },
+        ],
+      };
 
-    this.options = {
-      cutout: "30%",
-      plugins: {
-        legend: {
-          labels: {
-            color: textColor,
+      this.options = {
+        cutout: "30%",
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor,
+            },
           },
         },
-      },
-    };
+      };
+    });
   }
 }
